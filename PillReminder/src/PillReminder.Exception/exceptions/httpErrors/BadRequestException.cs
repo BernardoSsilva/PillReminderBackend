99656905f1c2718486a505380e7393f300882a34
bernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-07-23 11:56:06 -0300

    :sparkles: Feat: Created Bad request exception

---
+++ PillReminder/src/PillReminder.Exception/exceptions/httpErrors/BadRequestException.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace PillReminder.Exception.exceptions.httpErrors
+{
+    public class BadRequestException : AppException
+    {
+        private string Error { get; set; }
+        public BadRequestException(string message) : base(message)
+        {
+            Error = message;
+        }
+        public override int statusCode => (int)HttpStatusCode.BadRequest;
+
+        public override List<string> getErrors()
+        {
+            return [Error];
+        }
+    }
+}
