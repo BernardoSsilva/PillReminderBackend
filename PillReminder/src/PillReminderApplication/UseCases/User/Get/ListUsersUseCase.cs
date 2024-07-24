fb4c6388012dbfc3116ae0fcf5921ddb11591e42
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-07-23 22:52:42 -0300

    :sparkles: Feat: Created find all users use case

---
+++ PillReminder/src/PillReminderApplication/UseCases/User/Get/ListUsersUseCase.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using PillReminder.Comunication.users.Responses;
+using PillReminder.Infrastructure.DataAccsess.Repositories;
+using PillReminderApplication.UseCases.User.Get.Interfaces;
+
+namespace PillReminderApplication.UseCases.User.Get
+{
+    internal class ListUsersUseCase : IListUsersUseCase
+    {
+        private readonly IMapper _mapper;
+        private readonly UserRepository _repository;
+
+        public ListUsersUseCase(UserRepository repository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+        public async Task<MultipleUserJsonResposne> Execute()
+        {
+            var usersList =await _repository.FindAllUsers();
+            var response = new MultipleUserJsonResposne {
+
+                UsersList = _mapper.Map<List<UserShortJsonResponse>>(usersList)
+            };
+            return response;
+        }
+    }
+}
