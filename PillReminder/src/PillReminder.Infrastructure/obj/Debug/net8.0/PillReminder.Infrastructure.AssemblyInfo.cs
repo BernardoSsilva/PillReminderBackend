fc82ca84a5a2aae6111f164c33cea608b6f47b02
bernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-07-25 12:04:01 -0300

    merge

--- PillReminder/src/PillReminder.Infrastructure/obj/Debug/net8.0/PillReminder.Infrastructure.AssemblyInfo.cs
+++ PillReminder/src/PillReminder.Infrastructure/obj/Debug/net8.0/PillReminder.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PillReminder.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3134c8b586b25632ad3e14e19384492ed1ea9bf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa12067fc6c99322e9ead0ae3744e97435501adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PillReminder.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PillReminder.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
