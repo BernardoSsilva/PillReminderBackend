6e3189e66d5fd6b1ee22c178f373bd86108608d9
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-07-28 00:03:32 -0300

    :sparkles: Feat: Created register user route

---
+++ PillReminder/src/PillReminder.API/controllers/User/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PillReminder.Comunication.users.Requests;
+using PillReminder.Comunication.users.Responses;
+using PillReminderApplication.UseCases.User.Post.Interfaces;
+using System.Xml.Linq;
+
+namespace PillReminder.API.controllers.User
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<IActionResult> registerNewUser([FromBody] UserJsonRequest requestBody, [FromServices] IRegisterUserUseCase useCase) {
+
+            var result = await useCase.Execute(requestBody);
+
+            return Created(string.Empty, result);
+        }
+    }
+}
